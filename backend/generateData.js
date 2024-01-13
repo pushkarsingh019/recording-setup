1acff0ee4a7416a862ba11ea5909f7142766cd94
Pushkar Singh <stoazest@gmail.com>
2024-01-13 17:48:56 +0530

    backend code update

--- backend/generateData.js
+++ backend/generateData.js
@@ -2,46 +2,38 @@ import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { createObjectCsvWriter } from 'csv-writer';
+
 import Trial from './database.js';
 
-function getThreeLetterMonth(month) {
-    const monthsAbbreviation = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    return monthsAbbreviation[month - 1];
-}
+const currentFilePath = fileURLToPath(import.meta.url);
+const dataFolderPath = path.join(path.dirname(currentFilePath), 'data');
+const filePath = path.join(dataFolderPath, 'final_data.csv');
+
+const csvWriter = createObjectCsvWriter({
+  path: filePath,
+  header: [
+    {id: 'startTiming', title: 'Start Timing'},
+    {id: 'endTiming', title: 'End Timing'}, 
+    {id: 'reactionTime', title: 'Reaction Time'}
+  ]
+});
 
 async function generateCsv(data) {
-    // Generate dynamic file name based on the current date and time
-    const currentDatetime = new Date();
-    const monthAbbreviation = getThreeLetterMonth(currentDatetime.getMonth() + 1);
 
-    // Get the directory path using fileURLToPath and dirname
-    const currentFilePath = fileURLToPath(import.meta.url);
-    const dataFolderPath = path.join(path.dirname(currentFilePath), 'data');
+  await fs.mkdir(dataFolderPath, {recursive: true});
 
-    // Create the data folder if it doesn't exist
-    await fs.mkdir(dataFolderPath, { recursive: true });
+  // Write data to the CSV file
+  try {
+    await csvWriter.writeRecords(data);
+    console.log(`CSV file created successfully: ${filePath}`);
+    
+    // Clear database after writing to CSV
+    await Trial.deleteMany({}); 
 
-    const fileName = `data_${currentDatetime.getFullYear()}_${monthAbbreviation}_${currentDatetime.getDate().toString().padStart(2, '0')}_${currentDatetime.getHours().toString().padStart(2, '0')}_${currentDatetime.getMinutes().toString().padStart(2, '0')}.csv`;
+  } catch (error) {
+    console.error('Error writing CSV:', error);
+  }
 
-    const filePath = path.join(dataFolderPath, fileName);
-
-    const csvWriter = createObjectCsvWriter({
-        path: filePath,
-        header: [
-            { id: 'startTiming', title: 'Start Timing' },
-            { id: 'endTiming', title: 'End Timing' },
-            { id: 'reactionTime', title: 'Reaction Time' }
-        ]
-    });
-
-    // Write data to the CSV file
-    try {
-        await csvWriter.writeRecords(data);
-        console.log(`CSV file created successfully: ${filePath}`);
-        await Trial.deleteMany({});
-    } catch (error) {
-        console.error('Error writing CSV:', error);
-    }
 }
 
-export default generateCsv
+export default generateCsv;
